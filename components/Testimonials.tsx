4986745e49b937de066ed9dc5ac68a10b5f87137
neverxpected <ibiss55@gmail.com>
2026-03-04 15:27:13 -0600

    feat: Add Testimonials component to showcase client reviews with an animated reveal effect.

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -61,7 +61,7 @@ export default function Testimonials() {
                         Client Testimonials
                     </div>
                     <h2 className="text-3xl lg:text-5xl font-black text-white tracking-tight mb-4">
-                        Don&apos;t take our{' '}
+                        Don&apos;t take our<br className="sm:hidden" />{' '}
                         <span style={{
                             background: 'linear-gradient(90deg, #9B30FF, #FF2D78, #9B30FF)',
                             WebkitBackgroundClip: 'text',
